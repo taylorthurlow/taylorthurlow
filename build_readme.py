f1a44ed3c263e60346550f0636f836da06b66cd0
Taylor Thurlow <taylorthurlow@me.com>
2020-07-11 21:32:55 -0700

    Update readme builder for my own data

--- build_readme.py
+++ build_readme.py
@@ -10,7 +10,7 @@ root = pathlib.Path(__file__).parent.resolve()
 client = GraphqlClient(endpoint="https://api.github.com/graphql")
 
 
-TOKEN = os.environ.get("SIMONW_TOKEN", "")
+TOKEN = os.environ.get("USER_TOKEN", "")
 
 
 def replace_chunk(content, marker, chunk, inline=False):
@@ -94,16 +94,8 @@ def fetch_releases(oauth_token):
     return releases
 
 
-def fetch_tils():
-    sql = "select title, url, created_utc from til order by created_utc desc limit 5"
-    return httpx.get(
-        "https://til.simonwillison.net/til.json",
-        params={"sql": sql, "_shape": "array",},
-    ).json()
-
-
 def fetch_blog_entries():
-    entries = feedparser.parse("https://simonwillison.net/atom/entries/")["entries"]
+    entries = feedparser.parse("https://thurlow.io/feed.xml")["entries"]
     return [
         {
             "title": entry["title"],
@@ -147,19 +139,7 @@ if __name__ == "__main__":
     )
     project_releases.open("w").write(project_releases_content)
 
-    tils = fetch_tils()
-    tils_md = "\n".join(
-        [
-            "* [{title}]({url}) - {created_at}".format(
-                title=til["title"],
-                url=til["url"],
-                created_at=til["created_utc"].split("T")[0],
-            )
-            for til in tils
-        ]
-    )
-    rewritten = replace_chunk(rewritten, "tils", tils_md)
-
+    # Blog entries
     entries = fetch_blog_entries()[:5]
     entries_md = "\n".join(
         ["* [{title}]({url}) - {published}".format(**entry) for entry in entries]
